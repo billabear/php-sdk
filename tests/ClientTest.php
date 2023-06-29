adc74c50b0ff50face311ca891ffebccbce7290f
Iain <iain@humblyarrogant.io>
2023-06-29 21:03:59 +0200

    Update

---
+++ tests/ClientTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * The PHP SDK for BillaBear.
+ * Copyright (C) 2023  Humbly Arrogant Software Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Tests\BillaBear\PhpSdk;
+
+use BillaBear\PhpSdk\Client;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function testCreatesInstance()
+    {
+        $client = new Client();
+        $this->assertInstanceOf(Client::class, $client);
+    }
+}
