5516f048fda9e4418082f3740382670135520fcc
Iain <iain@humblyarrogant.io>
2023-06-30 23:50:04 +0200

    Add test for response body

--- tests/RequestSenderTest.php
+++ tests/RequestSenderTest.php
@@ -87,4 +87,36 @@ class RequestSenderTest extends TestCase
 
         $this->assertNull($response);
     }
+
+    public function testSendRequestBodyResponseBody()
+    {
+        $url = 'https://localhost';
+        $apiKey = 'api-key';
+        $method = 'POST';
+        $requestUrl = '/api/v1/info';
+        $body = ['content' => 'here'];
+        $responseBody = ['success' => 100];
+
+        $client = $this->createMock(ClientInterface::class);
+        $requestFactory = $this->createMock(RequestFactoryInterface::class);
+        $streamFactory = $this->createMock(StreamFactoryInterface::class);
+        $request = $this->createMock(RequestInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+        $stream = $this->createMock(StreamInterface::class);
+        $streamBody = $this->createMock(StreamInterface::class);
+
+        $requestFactory->method('createRequest')->with($method, $url.$requestUrl)->willReturn($request);
+        $request->method('withHeader')->with('X-API-KEY', $apiKey)->willReturnSelf();
+        $streamFactory->method('createStream')->with(json_encode($body))->willReturn($streamBody);
+        $request->expects($this->once())->method('withBody')->with($streamBody)->willReturnSelf();
+        $client->expects($this->once())->method('sendRequest')->with($request)->willReturn($response);
+        $response->method('getStatusCode')->willReturn(200);
+        $response->method('getBody')->willReturn($stream);
+        $stream->method('getContents')->willReturn(json_encode($responseBody));
+
+        $requestSender = new RequestSender($apiKey, $url, $client, $requestFactory, $streamFactory);
+        $actual = $requestSender->send($method, $requestUrl, $body);
+
+        $this->assertEquals($responseBody, $actual);
+    }
 }
